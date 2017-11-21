cc44deb7740e84ef593f83eb2106a7edba27caa0
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2017-11-21 18:06:37 +0800

    change tuition computation

--- web/index.php
+++ web/index.php
@@ -413,6 +413,7 @@
                     <a href="#" class="dream-dot" data-num="8"><span>4</span></a>
                     <a href="#" class="dream-dot" data-num="9"><span>5</span></a>
                     <a href="#" class="dream-dot" data-num="10"><span>5</span></a>
+                    
                 </div>
             </div>
         </section>
@@ -1082,10 +1083,7 @@
             var start_tuition = Math.round(annual_tuition*Math.pow(1+inflate_rate,(18-childs_age)));
             
             total_tuition_in_n_years = start_tuition;
-            for(var i=1;i < n_years_in_college; i++){
-                total_tuition_in_n_years += Math.round(annual_tuition*Math.pow(1+inflate_rate,(18+i)-childs_age));
-            }
-            console.log('Start tuition '+start_y+' Php'+start_tuition)
+            total_tuition_in_n_years = Math.round(total_tuition_in_n_years*4);
             $('#pj-year').html(start_y+'-'+(start_y+3))
             $('#result').html('Php '+Math.round(total_tuition_in_n_years).toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
         }
